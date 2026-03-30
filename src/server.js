84b702166e0382ee77e5c1f37a43662ac3d33570
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-03-30 07:14:55 -0300

    LInkando rotas da API com o SERVER

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "http";
 
+import { routes } from "./routes.js";
+
 const server = http.createServer((request, response) => {
-  response.end("Hello, World!");
+  const route = routes.find(
+    (target) => request.method === target.method && request.url === target.path,
+  );
+
+  if (route) {
+    return route.handler(request, response);
+  }
+
+  response.writeHead(404).end();
 });
 
 server.listen(3000);
