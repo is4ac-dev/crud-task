9dff5118528ee68215404009bb43fb7bfdfb4efa
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-03-30 07:30:59 -0300

    Arquitetando insert do Database

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,27 @@
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+
+const database = new Database();
+
 export const routes = [
   {
     method: "POST",
     path: "/tasks",
     handler(request, response) {
-      response.writeHead(201).end("Tarefa criada!");
+      const task = {
+        id: randomUUID(),
+        title: "Atualização Windows",
+        description: "Atualizar o OS de Windows 10 para Windows 11",
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: null,
+      };
+
+      database.insert("tasks", task);
+
+      response
+        .writeHead(201, { "Content-type": "application/json " })
+        .end(JSON.stringify(task));
     },
   },
 ];
